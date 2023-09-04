205b3bd9e7e4736803dc00cb133e970fbad0d216
mohamadmahdi <135762439+mohamadhosseinpour16@users.noreply.github.com>
2023-09-04 16:57:18 +0330

    Add the awesome font file to the project and write the relevant functions and add icons to the page in the render function

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,28 @@
 // select dom node
 let add = document.getElementById("Add");
 let root = document.getElementById("root");
+let input = document.querySelector("input");
 
 // functions
-const handleAdd = () => {};
+const handleClickAdd = () => {};
+const handleClickDelete = () => {};
+const handleClickEdit = () => {};
+const handleClickSuccess = () => [];
 
 const render = (todolist) => {
   let template = todos.map((todos) => {
     const { title } = todos;
-    return `<li>${title}</li>`;
+    return `
+         <li class="List">${title}
+                     <i onclick="handleClickDelete()" class="fa-solid fa-trash" style="color: #000000;"></i>
+                     <i onclick="handleClickEdit()" class="fas fa-edit" style="color: #000000;"></i>
+                     <i onclick="handleClickSuccess()" class="fa-solid fa-check" style="color: #000000;"></i>
+         </li>
+        `;
   });
   root.innerHTML += template.join("");
 };
 render();
 
 // events
-add.addEventListener("click", handleAdd);
+add.addEventListener("click", handleClickAdd);
